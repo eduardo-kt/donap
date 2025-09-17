ff840b6dbde2ade5a95a73bfc692c9f938c8017b
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-09-17 15:23:57 -0300

    feat(persistencia): adiciona funções carregar_donatarios e salvar_donatarios
    - carregar_donatarios: recupera dados de donatarios.json
    - salvar_donatarios: salva registro de donatário em donatarios.json

--- src/persistencia.py
+++ src/persistencia.py
@@ -18,3 +18,17 @@ def salvar_json(arquivo, dados):
     path = Path("data") / arquivo
     with open(path, "w", encoding="utf-8") as f:
         json.dump(dados, f, indent=4, ensure_ascii=False)
+
+
+# -------- Funções específicas --------
+ARQ_DONATARIOS = "donatarios.json"
+
+
+def carregar_donatarios():
+    return carregar_json(ARQ_DONATARIOS)
+
+
+def salvar_donatario(donatario):
+    donatarios = carregar_donatarios()
+    donatarios.append(donatario)
+    salvar_json(ARQ_DONATARIOS, donatarios)
